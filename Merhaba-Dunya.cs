dfd1b3b50cf5d23fd8daf612df4a407c12bbeb5f
Mehmet Ali <mhmtaliongn@gmail.com>
2017-07-29 02:35:53 +0300

    C# Dilinde Merhaba Dunya
    Mehmet Ali tarafindan kodlanmistir

---
+++ Merhaba-Dunya.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rextester
+{
+    public class MerhabaDunya
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+        }
+    }
+}
